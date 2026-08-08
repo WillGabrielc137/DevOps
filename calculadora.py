cfc052813bd4de316e4bb07196e40d6305a061f3
Heitor <heitormfm@gmail.com>
2026-08-07 22:06:14 -0300

    feat: Adiciona o cupom de desconto

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,6 @@
-def calcular_total(itens, desconto_percentual=0):
-    """
-    Calcula o total de uma compra.
+from desconto import obterDescontoCupom
 
-    Cada item representa uma tupla no formato:
-    (preco_unitario, quantidade)
-    """
+def calcular_total(itens, desconto_percentual = 0, cupom = None):
     if not 0 <= desconto_percentual <= 100:
         raise ValueError("O desconto precisa estar entre 0 e 100.")
 
@@ -13,7 +9,12 @@ def calcular_total(itens, desconto_percentual=0):
         for preco_unitario, quantidade in itens
     )
 
-    desconto = subtotal * (desconto_percentual / 100)
+    descontoTotal = desconto_percentual + obterDescontoCupom(cupom)  
+    descontoTotal = min(descontoTotal, 100)
+     
+    desconto = subtotal * (descontoTotal / 100)
     total = subtotal - desconto
 
     return round(total, 2)
+
+
